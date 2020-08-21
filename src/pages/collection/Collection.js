f9dcfb22c3549f556fe5213617b200d13e9dd14f
manovarun <samvarun@gmail.com>
2020-08-21 09:58:58 +0530

    Spinner Updated

--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -3,20 +3,23 @@ import { connect } from "react-redux";
 import "./collection.scss";
 import { selectCollection } from "../../redux/shop/shop.selector";
 import CollectionItem from "../../components/collection-item/CollectionItem";
+import {
+  CollectionPageContainer,
+  CollectionTitle,
+  CollectionItemsContainer,
+} from "./collection.styles";
 
 const Collection = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
-        {items.length > 0 ? (
-          items.map((item) => <CollectionItem key={item.id} item={item} />)
-        ) : (
-          <p>No items found</p>
-        )}
-      </div>
-    </div>
+    <CollectionPageContainer>
+      <CollectionTitle>{title}</CollectionTitle>
+      <CollectionItemsContainer>
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </CollectionItemsContainer>
+    </CollectionPageContainer>
   );
 };
 
--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selector";
 import CollectionOverview from "../../components/collection-overview/CollectionOverview";
 import Collection from "../collection/Collection";
 import {
@@ -10,8 +8,16 @@ import {
   convertCollectionsSnapshotToMap,
 } from "../../firebase/firebase.utils.js";
 import { updateCollections } from "../../redux/shop/shop.actions.js";
+import WithSpinner from "../../components/with-spinner/WithSpinner";
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionWithSpinner = WithSpinner(Collection);
 
 class ShopPage extends React.Component {
+  state = {
+    loading: true,
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -27,22 +33,30 @@ class ShopPage extends React.Component {
 
   render() {
     const { match } = this.props;
+    const { isLoading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionOverview} />
-        <Route path={`${match.path}/:collectionId`} component={Collection} />
+        <Route
+          exact
+          path={`${match.path}`}
+          component={(props) => (
+            <CollectionOverviewWithSpinner {...props} isLoading={isLoading} />
+          )}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={(props) => (
+            <CollectionWithSpinner {...props} isLoading={isLoading} />
+          )}
+        />
       </div>
     );
   }
 }
-
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
 
 const mapDispatchToProps = (dispatch) => ({
   updateCollections: (collectionsMap) =>
     dispatch(updateCollections(collectionsMap)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
+export default connect(null, mapDispatchToProps)(ShopPage);
